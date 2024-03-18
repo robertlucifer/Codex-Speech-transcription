21b4d9564e91a59e9a83d734c89bf6fb868b8f00
Robert Antony <antonyrobert202@gmail.com>
2024-03-18 17:22:13 +0530

    completed the major parts remaining small changes

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import speech_recognition as sr
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    transcript = ""
     if request.method == 'POST':
-        print("Data received")
         if "file" not in request.files:
+
             return redirect(request.url)
         file = request.files["file"]
         if file.filename == "":
@@ -15,11 +16,11 @@ def index():
             audioFile = sr.AudioFile(file)
             with audioFile as source:
                 data = recognizer.record(source)
-            text = recognizer.recognize_google(data, key=None)
-            print(text)
+            transcript = recognizer.recognize_google(data, key=None)
+            print(transcript)
 
 
-        return render_template('index.html', data=request.form)
-    return render_template('index.html')
+        return render_template('index.html', transcript=transcript)
+    return render_template('index.html', transcript=transcript)
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
