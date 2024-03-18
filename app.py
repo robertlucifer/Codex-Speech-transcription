7c1df1eaf788220592f9b5476bc51de590ab2050
Robert Antony <antonyrobert202@gmail.com>
2024-03-18 15:21:35 +0530

    added the transcription module

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         print("Data received")
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            text = recognizer.recognize_google(data, key=None)
+            print(text)
+
+
         return render_template('index.html', data=request.form)
     return render_template('index.html')
 if __name__ == '__main__':
