d5d74a6c141b20ac33c1d1539818f3272d6dc981
Robert Antony <antonyrobert202@gmail.com>
2024-03-18 14:51:00 +0530

    leanred how flask works and created a form post and get using flask and HTML

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print("Data received")
+        return render_template('index.html', data=request.form)
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
